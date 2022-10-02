7893d313ca8ce47f9a991556178fdcaf1c1042d0
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-10-02 07:50:28 -0500

    Adding Listing-5-7

---
+++ Listing-5-7/rules.c
@@ -0,0 +1,14 @@
+// rules.c -- applies precedence test
+
+#include <stdio.h>
+
+int main() {
+
+	int top, score;
+
+	top = score = -(2 + 5) * 6 + (4 + 3 * (2 + 3));
+	printf("top = %d \n", top);
+
+	printf("\n");
+	return 0;
+}
